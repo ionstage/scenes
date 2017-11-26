3a375e8bf23e64a0da3233529d0589c33b355871
nasitra <nasitra@users.noreply.github.com>
2017-11-26 17:15:34 +0900

    Add module for utility methods

---
+++ js/helper.js
@@ -0,0 +1,11 @@
+(function(app) {
+  'use strict';
+
+  var helper = {};
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = helper;
+  } else {
+    app.helper = helper;
+  }
+})(this.app || (this.app = {}));
