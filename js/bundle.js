e3b81329c41a485d30b9823bb6e8dbe72cbcee99
nasitra <nasitra@users.noreply.github.com>
2017-11-27 20:49:39 +0900

    Use 'jcore' library

---
+++ js/bundle.js
@@ -0,0 +1,105 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"jcore":[function(require,module,exports){
+/**
+ * jCore v0.2.0
+ * (c) 2016 iOnStage
+ * Released under the MIT License.
+ */
+
+(function(global) {
+  'use strict';
+
+  if (!global.requestAnimationFrame) {
+    global.requestAnimationFrame = function(callback) {
+      return setTimeout(callback, 1000 / 60);
+    };
+  }
+
+  var lastIndexOf = function(array, value) {
+    for (var i = array.length - 1; i >= 0; i--) {
+      if (array[i] === value)
+        return i;
+    }
+
+    return -1;
+  };
+
+  var Component = function() {
+    this.relations = [];
+  };
+
+  Component.prototype.prop = function(initialValue, defaultValue, converter) {
+    var hasConverter = (typeof converter === 'function');
+    var cache = hasConverter ? converter(initialValue, defaultValue) : initialValue;
+
+    return function(value) {
+      if (typeof value === 'undefined')
+        return cache;
+
+      if (value === cache)
+        return;
+
+      cache = hasConverter ? converter(value, cache) : value;
+
+      this.markDirty();
+    };
+  };
+
+  Component.prototype.redraw = function() {};
+
+  Component.prototype.markDirty = (function() {
+    var dirtyComponents = [];
+    var requestId = null;
+
+    var updateRelations = function(index) {
+      for (var ci = index, clen = dirtyComponents.length; ci < clen; ci++) {
+        var component = dirtyComponents[ci];
+        var relations = component.relations;
+
+        for (var ri = 0, rlen = relations.length; ri < rlen; ri++) {
+          relations[ri].update(component);
+        }
+      }
+
+      // may be inserted other dirty components by updating relations
+      if (dirtyComponents.length > clen)
+        updateRelations(clen);
+    };
+
+    var callback = function() {
+      updateRelations(0);
+
+      for (var i = 0, len = dirtyComponents.length; i < len; i++) {
+        dirtyComponents[i].redraw();
+      }
+
+      dirtyComponents = [];
+      requestId = null;
+    };
+
+    return function() {
+      if (lastIndexOf(dirtyComponents, this) === -1)
+        dirtyComponents.push(this);
+
+      if (requestId !== null)
+        return;
+
+      requestId = global.requestAnimationFrame(callback);
+    };
+  })();
+
+  var Relation = function() {};
+
+  Relation.prototype.update = function() {};
+
+  var jCore = {
+    Component: Component,
+    Relation: Relation
+  };
+
+  if (typeof module !== 'undefined' && module.exports)
+    module.exports = jCore;
+  else
+    global.jCore = jCore;
+})(this);
+
+},{}]},{},[]);
