7e2a4d0b04a39f7721146462e4ea6a19161f88e5
nasitra <nasitra@users.noreply.github.com>
2018-02-04 16:09:39 +0900

    Create 'controls' component

--- js/components/main.js
+++ js/components/main.js
@@ -2,8 +2,11 @@
   'use strict';
 
   var jCore = require('jcore');
+  var Controls = app.Controls || require('./controls.js');
 
-  var Main = jCore.Component.inherits();
+  var Main = jCore.Component.inherits(function() {
+    this.controls = new Controls({ element: this.findElement('.controls') });
+  });
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Main;
