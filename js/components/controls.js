ed92b3e03b2ba4164d9af415e81dc4b6ba4652e5
nasitra <nasitra@users.noreply.github.com>
2018-02-11 09:32:55 +0900

    Add 'next' event of the controls

--- js/components/controls.js
+++ js/components/controls.js
@@ -2,8 +2,15 @@
   'use strict';
 
   var jCore = require('jcore');
+  var Button = app.Button || require('./button.js');
 
-  var Controls = jCore.Component.inherits();
+  var Controls = jCore.Component.inherits(function() {
+    this.nextButton = new Button({ element: this.findElement('.next-button') });
+  });
+
+  Controls.prototype.oninit = function() {
+    this.nextButton.on('tap', this.emit.bind(this, 'next'));
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controls;
