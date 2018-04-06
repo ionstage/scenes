f3468759dee2426d84ab252c13ea30e32e796d44
nasitra <nasitra@users.noreply.github.com>
2018-04-07 08:10:42 +0900

    Add method to change the foreground

--- js/components/foreground.js
+++ js/components/foreground.js
@@ -2,8 +2,31 @@
   'use strict';
 
   var jCore = require('jcore');
+  var dom = app.dom || require('../dom.js');
 
   var Foreground = jCore.Component.inherits();
+
+  Foreground.prototype.change = function(background, opacity) {
+    return new Promise(function(resolve) {
+      var children = dom.children(this.element());
+      dom.once(this.element(), 'transitionend', function() {
+        dom.append(this.element(), children[0]);
+        dom.css(children[1], { background: background });
+        resolve();
+      }.bind(this));
+      dom.css(children[0], {
+        background: background,
+        opacity: (typeof opacity !== 'undefined' ? opacity : 1),
+      });
+      dom.css(children[1], { opacity: 0 });
+    }.bind(this));
+  };
+
+  Foreground.prototype.oninit = function() {
+    var children = dom.children(this.element());
+    dom.css(children[0], { opacity: 0 });
+    dom.css(children[1], { opacity: 1 });
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Foreground;
