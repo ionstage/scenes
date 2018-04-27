783234d52e10ac37a773ecb33eb612f1913b30a4
nasitra <nasitra@users.noreply.github.com>
2018-04-28 08:34:49 +0900

    Make property for setting z-index of content-item

--- js/components/content-item.js
+++ js/components/content-item.js
@@ -6,11 +6,16 @@
   var ContentItem = jCore.Component.inherits(function(props) {
     this.x = this.prop(props.x);
     this.y = this.prop(props.y);
+    this.z = this.prop(props.z || 0);
   });
 
   ContentItem.prototype.onredraw = function() {
     this.redrawBy('x', 'y', function(x, y) {
       dom.translate(this.element(), x, y);
+    });
+
+    this.redrawBy('z', function(z) {
+      dom.css(this.element(), { zIndex: parseInt(z * 100, 10) });
     });
   };
 
