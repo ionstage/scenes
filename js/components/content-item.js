37d248d216fcecd707782a8d11dd0e54f6b6cdc7
nasitra <nasitra@users.noreply.github.com>
2018-04-29 18:15:47 +0900

    Make method to load a content of content-item

--- js/components/content-item.js
+++ js/components/content-item.js
@@ -2,12 +2,19 @@
   'use strict';
 
   var jCore = require('jcore');
+  var dom = app.dom || require('../dom.js');
 
   var ContentItem = jCore.Component.inherits(function(props) {
     this.x = this.prop(props.x);
     this.y = this.prop(props.y);
     this.z = this.prop(props.z || 0);
   });
+
+  ContentItem.prototype.load = function(url) {
+    return dom.ajax({ type: 'GET', url: url }).then(function(text) {
+      dom.append(this.element(), dom.render(text));
+    }.bind(this));
+  };
 
   ContentItem.prototype.onredraw = function() {
     this.redrawBy('x', 'y', function(x, y) {
